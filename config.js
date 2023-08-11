04cb8c2499bc8f64c3e328ac581f51b3102c3864
Luhan Honnywey <honnywey@gmail.com>
2023-08-10 21:48:28 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 module.exports = {
-    apiKey: "luhan3:bd892f56386ebabb0bbb6b9aa298d429-us18",
-    listId: "https://us18.api.mailchimp.com/3.0/be7de78e60"
+    apiKey: "APIKEY",
+    listId: "LISTID"
 };
